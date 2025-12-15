80725574ce2ecba116f081e59dce8b1ca28edb07
Farzad <Farzadsarfarazwvo@gmail.com>
2025-12-15 03:39:22 +0100

    type management

--- lib/auth.ts
+++ lib/auth.ts
@@ -48,8 +48,8 @@ export const authOptions: NextAuthOptions = {
         return {
           id: user.id,
           email: user.email,
-          name: user.name,
-          image: user.image,
+          name: user.name ?? undefined,   
+          image: user.image ?? undefined,
         };
       }
     })
